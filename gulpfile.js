59b6a5cb70e8081df58b1799026cd2e3fbe8c34c
Andrukas8 <rukasav@gmail.com>
2018-03-12 08:22:48 +0200

    added postcss and gulp features

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-  watch = require('gulp-watch');
+  watch = require('gulp-watch'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssvars = require('postcss-simple-vars'),
+  nested = require('postcss-nested');
 
 
 gulp.task('default', function () {
@@ -13,17 +17,19 @@ gulp.task('html', function () {
 
 
 gulp.task('styles', function () {
-  console.log("Imagine Sass or PostCSS tasks running here.");
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
 });
 
 
-gulp.task('watch', function(){
-  
-  watch('./app/index.html', function(){
+gulp.task('watch', function () {
+
+  watch('./app/index.html', function () {
     gulp.start('html');
   });
 
-  watch('./app/assets/styles/**/*.css', function(){
+  watch('./app/assets/styles/**/*.css', function () {
     gulp.start('styles');
   });
 
