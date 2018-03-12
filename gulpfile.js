4065fd501c9a30cc9bfce7e4c10a949796723fcb
Andrukas8 <rukasav@gmail.com>
2018-03-12 11:20:08 +0200

    added BEM style css architecture

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,18 @@ var gulp = require('gulp'),
   postcss = require('gulp-postcss'),
   autoprefixer = require('autoprefixer'),
   cssvars = require('postcss-simple-vars'),
-  nested = require('postcss-nested');
-
-
-gulp.task('default', function () {
-  console.log("Hooray, you created a gulp task");
-});
-
-
-gulp.task('html', function () {
-  console.log("Imagine something useful being done to your html here");
-});
+  nested = require('postcss-nested'),
+  cssImport = require('postcss-import');
 
 
 gulp.task('styles', function () {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
 
 gulp.task('watch', function () {
-
-  watch('./app/index.html', function () {
-    gulp.start('html');
-  });
 
   watch('./app/assets/styles/**/*.css', function () {
     gulp.start('styles');
