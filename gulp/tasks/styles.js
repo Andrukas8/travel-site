da56f80d60f94152f11dcc19009ded9c55aa49c2
Andrukas8 <rukasav@gmail.com>
2018-03-13 10:01:22 +0200

    divided big gulp file into task files

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssvars = require('postcss-simple-vars'),
+  nested = require('postcss-nested'),
+  cssImport = require('postcss-import');
+
+gulp.task('styles', function () {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
