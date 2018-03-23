ec3894939ce5808ca55f087f644e017a6440d64e
Andrukas8 <rukasav@gmail.com>
2018-03-23 09:43:46 +0200

    Styling the header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ var gulp = require('gulp'),
   cssvars = require('postcss-simple-vars'),
   nested = require('postcss-nested'),
   cssImport = require('postcss-import'),
-  mixins = require('postcss-mixins');
+  mixins = require('postcss-mixins'),
+  hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function () {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
     .on('error', function (errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
